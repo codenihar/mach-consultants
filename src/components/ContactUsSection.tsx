7b3db433de2662101f83f6d1b3572e95b8f96789
Hruthik <mhrithik450@gmail.com>
2025-06-04 20:37:07 +0530

    updated contact section

--- src/components/ContactUsSection.tsx
+++ src/components/ContactUsSection.tsx
@@ -7,7 +7,7 @@ const ConsultationForm: React.FC = () => {
       <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-2 items-center gap-8 md:gap-10 lg:gap-12">
         {/* Image Section */}
         <div className="order-2 lg:order-1 w-full">
-          <div className="relative w-full aspect-[4/5] lg:aspect-[9/10] rounded-xl overflow-hidden shadow-lg">
+          <div className="relative w-full aspect-[4/5] lg:aspect-[3/4] rounded-xl overflow-hidden shadow-lg">
             <img
               src="/images/consulting_image.png"
               alt="Consultation"
@@ -26,7 +26,7 @@ const ConsultationForm: React.FC = () => {
             <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 text-gray-900 leading-tight">
               Contact us now for a free consultation
             </h2>
-            <p className="text-gray-700 my-4 sm:my-6 text-base sm:text-lg">
+            <p className="text-gray-700 my-4 sm:my-0 text-base sm:text-md">
               Consultic can handle tech advisory help decide upon the project
               rationale. Leave your contacts below and we'll get back to you
               within 24 hours.
@@ -43,31 +43,70 @@ const ConsultationForm: React.FC = () => {
               </div>
               <div className="max-sm:col-span-2">
                 <input
+                  type="text"
+                  placeholder="Last name"
+                  className="w-full p-3 rounded-lg border border-gray-300 bg-white text-gray-900 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                  required
+                />
+              </div>
+              <div className="col-span-2">
+                <input
+                  type="text"
+                  placeholder="Organization"
+                  className="w-full p-3 rounded-lg border border-gray-300 bg-white text-gray-900 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                  required
+                />
+              </div>
+              <div>
+                <input
                   type="email"
                   placeholder="Email address*"
                   className="w-full p-3 rounded-lg border border-gray-300 bg-white text-gray-900 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                   required
                 />
               </div>
+              <div>
+                <input
+                  type="number"
+                  placeholder="Phone Number"
+                  className="w-full p-3 rounded-lg border border-gray-300 bg-white text-gray-900 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                  required
+                />
+              </div>
               <div className="col-span-2">
                 <select className="w-full p-3 rounded-lg border border-gray-300 bg-white text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent appearance-none">
-                  <option>Are you a new client?</option>
-                  <option>Yes</option>
-                  <option>No</option>
+                  <option>Inquiry type?</option>
+                  <option>Support or consulting</option>
+                  <option>Conference or speaking</option>
+                  <option>Collaboration or research</option>
+                  <option>Information request</option>
+                  <option>Other</option>
                 </select>
               </div>
               <div className="col-span-2">
                 <textarea
                   placeholder="How can we help you?"
-                  rows={4}
+                  rows={2}
                   className="w-full p-3 rounded-lg border border-gray-300 bg-white text-gray-900 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 ></textarea>
+              </div>
+              <div className="col-span-2 flex items-start gap-2 cursor-pointer">
+                <input
+                  id="checkbox"
+                  type="checkbox"
+                  className="cursor-pointer mt-1.5"
+                  required
+                />
+                <label htmlFor="checkbox" className="text-black">
+                  GDPR consent: I authorize MACH to use my data to respond to my
+                  request
+                </label>
               </div>
 
               <div className="max-sm:col-span-2 mt-2">
                 <button
                   type="submit"
-                  className="w-[max-content] px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition-colors duration-300 shadow-md hover:shadow-lg flex items-center justify-center gap-2"
+                  className="w-[max-content] px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition-colors duration-300 shadow-md hover:shadow-lg flex items-center justify-center gap-2 cursor-pointer"
                 >
                   Make An Appointment
                   <MoveUpRight className="w-4 h-4" />
